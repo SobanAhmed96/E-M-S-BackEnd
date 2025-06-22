ea1c53b4f7decc23eeff9dbd70e6b91ff53e8f8a
SobanAhmed96 <sobanahmed9898@gmail.com>
2025-06-22 19:17:36 +0500

    backend

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-    // credentials: true,
-    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
 );
 app.use(cookieParser());
